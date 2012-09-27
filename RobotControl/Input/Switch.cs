86a2ea9ee5ceb57db5cb6d7e3d65e58be510db60
Thomas Bomatter <thomas.bomatter@bluewin.ch>
2012-09-27 16:00:04 +0200

    Adding RobotControl files

---
+++ RobotControl/Input/Switch.cs
@@ -0,0 +1,85 @@
+﻿//------------------------------------------------------------------------------
+// S Y S T E M N A H E S   P R O G R A M M I E R E N   (P R G S Y)
+//------------------------------------------------------------------------------
+// Repository:
+//    $Id: Switch.cs 513 2011-02-17 15:17:16Z zajost $
+//------------------------------------------------------------------------------
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotCtrl
+{
+
+    public enum Switches
+    {
+        Switch1 = 0,
+        Switch2,
+        Switch3,
+        Switch4
+    }
+
+
+    /// <summary>
+    /// Diese Klasse bildet einen Schalter des Roboters ab
+    /// </summary>
+    public class Switch
+    {
+
+        #region members
+        private Switches swi;
+        private bool switchEnabled;
+        #endregion
+
+
+        #region eventhandler
+        public event EventHandler<SwitchEventArgs> SwitchStateChanged;
+        #endregion
+
+
+        #region constructor & destructor
+        /// <summary>
+        /// Initialisiert den Schalter.
+        /// </summary>
+        /// <param name="swi">der abzubildende Schalter</param>
+        public Switch(Switches swi)
+        {
+            this.swi = swi;
+            this.switchEnabled = false;
+        }
+        #endregion
+
+
+        #region properties
+        /// <summary>
+        /// Liefert bzw. setzt den Zustand des Schalters (ein-/ausgeschaltet)
+        /// </summary>
+        public bool SwitchEnabled
+        {
+            get { return switchEnabled; }
+            set
+            {
+                switchEnabled = value;
+                OnSwitchStateChanged(new SwitchEventArgs(this.swi, value));
+            }
+        }
+        #endregion
+
+
+        #region methods
+        /// <summary>
+        /// Diese Methode informiert alle registrierten Eventhandler über den Zustandswechsel 
+        /// (ein-/ausgeschaltet) des Schalters.
+        /// </summary>
+        public void OnSwitchStateChanged(SwitchEventArgs e)
+        {
+            if (SwitchStateChanged != null)
+            {
+                SwitchStateChanged(this, e);
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ RobotControl/Output/Led.cs
@@ -0,0 +1,88 @@
+﻿//------------------------------------------------------------------------------
+// S Y S T E M N A H E S   P R O G R A M M I E R E N   (P R G S Y)
+//------------------------------------------------------------------------------
+// Repository:
+//    $Id: Led.cs 513 2011-02-17 15:17:16Z zajost $
+//------------------------------------------------------------------------------
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotCtrl
+{
+
+    public enum Leds
+    {
+        Led1 = 0,
+        Led2,
+        Led3,
+        Led4
+    }
+
+
+    /// <summary>
+    /// Diese Klasse bildet eine LED des Roboters ab.
+    /// </summary>
+    public class Led
+    {
+
+        #region members
+        private Leds led;
+        private bool ledEnabled;
+        #endregion
+
+
+        #region eventhandler
+        public event EventHandler<LedEventArgs> LedStateChanged;
+        #endregion
+
+
+        #region constructor & destructor
+        /// <summary>
+        /// Initialisiert die gewünschte LED und verknüpft sie mit einem digitalOut-Objekt.
+        /// </summary>
+        /// <param name="digitalOut"></param>
+        /// <param name="led"></param>
+        public Led(Leds led)
+        {
+            this.led = led;
+        }
+        #endregion
+
+
+        #region properties
+        /// <summary>
+        /// Liefert bzw. setzt den Zustand der LED (ein-/ausgeschaltet)
+        /// </summary>
+        public bool LedEnabled
+        {
+            get { return this.ledEnabled; }
+            set
+            {
+                if (value != ledEnabled)
+                {
+                    ledEnabled = value;
+                    OnLedStateChanged();
+                }
+            }
+        }
+        #endregion
+
+
+        #region methods
+        /// <summary>
+        /// Diese Methode informiert alle registrierten Eventhandler über den Zustandswechsel 
+        /// (ein-/ausgeschaltet) der LED.
+        /// </summary>
+        public void OnLedStateChanged()
+        {
+            if (LedStateChanged != null)
+            {
+                LedStateChanged(this, new LedEventArgs(this.led, this.ledEnabled));
+            }
+        }
+        #endregion
+
+    }
+}
