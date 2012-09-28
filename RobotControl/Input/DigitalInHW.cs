88804d582c946918ac5f02889d2ef2213ff182c0
Thomas Bomatter <thomas.bomatter@bluewin.ch>
2012-09-28 16:11:04 +0200

    Adding HW Support

---
+++ RobotControl/Input/DigitalInHW.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using RobotIO;
+
+namespace RobotControl.Input
+{
+    class DigitalInHW : DigitalIn
+    {
+        private readonly int _ioPort;
+        private readonly Timer _pollTimer;
+        private int _oldData;
+        private bool _isDisposed;
+
+        public DigitalInHW(int ioPort) {
+            _ioPort = ioPort;
+            _pollTimer = new Timer(PollTimerCallback, null, 0, 50);
+            _oldData = 0;
+        }
+
+        private void PollTimerCallback(object state) {
+            int newData = Data;
+            if (_oldData != newData)
+            {
+                _oldData = newData;
+                OnDigitalInChanged(EventArgs.Empty);
+            }
+        }
+        
+        public override void Dispose() {
+            if (_isDisposed)
+            {
+                _isDisposed = true;
+                _pollTimer.Dispose();
+            }   
+        }
+
+        public override int Data
+        {
+            get { return IOPortEx.Read(_ioPort); } 
+            set {IOPortEx.Write(_ioPort, value); }
+        }
+    }
+}
---
+++ RobotControl/Output/DigitalOutHW.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using RobotIO;
+
+namespace RobotControl.Output
+{
+    class DigitalOutHW : DigitalOut
+    {
+        private readonly int _ioPort;
+
+        public DigitalOutHW(int ioPort) {
+            _ioPort = ioPort;
+        }
+
+        public override void Dispose() {
+        }
+
+        public override int Data {
+            get { return IOPortEx.Read(_ioPort); } 
+            set {
+                IOPortEx.Write(_ioPort, value);
+                OnDigitalOutChanged(EventArgs.Empty);
+            }
+        }
+    }
+}
