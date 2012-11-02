410a8583a65deb4e7c14cc5230a32e0365e4d633
Thomas Bomatter <thomas.bomatter@bluewin.ch>
2012-11-02 16:51:40 +0100

    Imporved Drive

--- RobotControl/DrivePatterns/BasePattern.cs
+++ RobotControl/DrivePatterns/BasePattern.cs
@@ -24,31 +24,40 @@ namespace RobotControl.DrivePatterns
       
     }
 
-    protected void RunLine(float length, float speed, float acceleration) {
+    protected void RunLine(float length, float speed, float acceleration, bool wait)
+    {
       if (_stop) throw new RobotStoppedException();
       World.Robot.Drive.RunLine(length, speed, acceleration);
-      _resetEvent.WaitOne();
+      if (wait) _resetEvent.WaitOne();
     }
 
-    protected void RunTurn(float angle, float speed, float acceleration)
+    protected void RunTurn(float angle, float speed, float acceleration, bool wait)
     {
       if (_stop) throw new RobotStoppedException();
       World.Robot.Drive.RunTurn(angle, speed, acceleration);
-      _resetEvent.WaitOne();
+      if (wait) _resetEvent.WaitOne();
     }
 
-    protected void RunArcRight(float radius, float angle, float speed, float acceleration)
+    protected void RunArcRight(float radius, float angle, float speed, float acceleration, bool wait)
     {
       if (_stop) throw new RobotStoppedException();
       World.Robot.Drive.RunArcRight(radius, angle, speed, acceleration);
-      _resetEvent.WaitOne();
+      if (wait) _resetEvent.WaitOne();
     }
 
-    protected void RunArcLeft(float radius, float angle, float speed, float acceleration)
+    /// <summary>
+    /// Runs the arc left.
+    /// </summary>
+    /// <param name="radius">The radius.</param>
+    /// <param name="angle">The angle.</param>
+    /// <param name="speed">The speed.</param>
+    /// <param name="acceleration">The acceleration.</param>
+    /// <exception cref="RobotControl.DrivePatterns.RobotStoppedException"></exception>
+    protected void RunArcLeft(float radius, float angle, float speed, float acceleration, bool wait)
     {
       if (_stop) throw new RobotStoppedException();
       World.Robot.Drive.RunArcLeft(radius, angle, speed, acceleration);
-      _resetEvent.WaitOne();
+      if (wait) _resetEvent.WaitOne();
     }
 
     public virtual void Stop() {
--- RobotControl/DrivePatterns/FindSpaceAndPark.cs
+++ RobotControl/DrivePatterns/FindSpaceAndPark.cs
@@ -19,32 +19,31 @@ namespace RobotControl.DrivePatterns
       try {
         Robot robot = World.Robot;
 
-        while (robot.Radar.Distance > 2.5f) {
-          RunLine(0.5f, 1, 1);
-        }
-        
-        while (robot.Radar.Distance < 2.5f) {
+        RunArcRight(0.5f, 90, 1, 1, true);
+        RunArcLeft(0.5f, 90, 1, 1, true);
+
+        /*while (robot.Radar.Distance < 2.5f) {
           RunTurn(10,1,1);
         }
 
         RunLine(1.25f, 1, 1);
 
-        RunTurn(-(90 - robot.Position.Angle), 1, 1);
+        RunTurn(-(90 - robot.Position.Angle), 1, 1);*/
 
-        RunLine(robot.Radar.Distance - 0.3f, 1, 1);
+        RunLine(robot.Radar.Distance - 0.3f, 1, 1, true);
 
-        RunTurn(-90, 1, 1);
+        RunTurn(-90, 1, 1, true);
 
         if (robot.Radar.Distance > 0.35f) {
           while (true) {
-            RunLine(0.5f, 1, 1);
+            RunLine(0.5f, 1, 1, true);
 
-            RunTurn(90, 1, 1);
+            RunTurn(90, 1, 1, true);
 
             if (robot.Radar.Distance < 0.35f) {
-              RunTurn(-90, 1, 1);
+              RunTurn(-90, 1, 1, true);
             } else {
-              RunLine(robot.Radar.Distance- 0.3f, 1, 1);
+              RunLine(robot.Radar.Distance - 0.3f, 1, 1, true);
               break;
             }
           }
